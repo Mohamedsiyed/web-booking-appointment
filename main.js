48f48830652761e94c0b6b04a236edda0f386deb
mohamed sayed <156014280+Mohamedsiyed@users.noreply.github.com>
2025-02-10 13:29:03 -0700

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-
- function bookSlot(cell) {
-    if (cell.classList.contains("available")) {
-        cell.classList.remove("available");
-        cell.classList.add("booked");
-        cell.textContent = "unavailable";
-    }
-}
