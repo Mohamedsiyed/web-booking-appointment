c46cd9ec315f3ad7e8f252dcd6f583c5c89d506b
mohamed sayed <156014280+Mohamedsiyed@users.noreply.github.com>
2025-02-10 13:30:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
             if (cell.classList.contains("available")) {
                 cell.classList.remove("available");
                 cell.classList.add("booked");
-                cell.innerHTML = '<i class="fas fa-times-circle"></i> Unavailable'; // إضافة أيقونة الغلط
+                cell.innerHTML = '<i class="fas fa-times-circle"></i> Unavailable'; 
             }
         }
         
